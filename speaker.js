d66f7bcb1b0a15b874ea634d4ebc16d29be1e1df
akyhn <ak@wsdesign.dk>
2022-08-20 14:24:07 +0200

    Update speaker.js

--- speaker.js
+++ speaker.js
@@ -3,7 +3,7 @@ let modalContainer = $("speaker-modal_wrapper").find(".speaker-modal_container")
 $(".event-speaker").on("click", function () {
   $(this).siblings(".speaker-modal_wrapper").addClass("is-open");
   $(this).siblings(".speaker-modal_overlay").addClass("is-open");
-  gsap.from($(this).siblings(modalContainer), {
+  gsap.from($(this).siblings(".speaker-modal_wrapper").find(".speaker-modal_container"), {
     opacity: 0,
     y: "-100%",
     duration: 0.8,
@@ -21,12 +21,3 @@ $(".speaker-modal_btn").on("click", function () {
 $(document).on("keyup", function (e) {
   if (e.key == "Escape") $(".speaker-modal_btn").click();
 });
-
-gsap.from(".event_container", {
-  opacity: 0,
-  x: -1000,
-  duration: 0.8,
-  ease: "power1.out",
-});
-
-gsap.set("body", { fill: "blue" });
