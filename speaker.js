200032ccf59c0ee83e9737dc5146e9f61e866c2e
akyhn <ak@wsdesign.dk>
2022-08-20 13:20:29 +0200

    Update speaker.js

--- speaker.js
+++ speaker.js
@@ -1,1 +1,36 @@
+// Speaker modal
+$(".event-speaker").on("click", function () {
+  $(this).siblings(".speaker-modal_wrapper").addClass("is-open");
+  $(this).siblings(".speaker-modal_overlay").addClass("is-open");
+  gsap.from($(this).siblings(".speaker-modal_container"), {
+    opacity: 0,
+    y: "-100%",
+    duration: 0.8,
+    delay: 0.4,
+    ease: "power1.out",
+  });
+});
 
+$(".speaker-modal_btn").on("click", function () {
+  $(".speaker-modal_overlay").removeClass("is-open");
+  $(".speaker-modal_wrapper").removeClass("is-open");
+  $(".speaker-modal_container").removeClass("is-open");
+});
+
+$(document).on("keyup", function (e) {
+  if (e.key == "Escape") $(".speaker-modal_btn").click();
+});
+
+gsap.fromTo(
+  $(this).siblings(".speaker-modal_container"),
+  {
+    opacity: 0,
+    y: "-100%",
+  },
+  {
+    opacity: 1,
+    y: "0%",
+    duration: 0.8,
+    ease: "power1.out",
+  }
+);
